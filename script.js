0a79d60856827eb0a3dba1d367a1c6df8d84abb2
Erik Lindebratt <erik.lindebratt@gmail.com>
2013-01-13 04:11:03 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,89 @@
+/**
+ * @author Erik Lindebratt
+ */
+var
+  mySounds = [],
+  soundCloudClientID = "3360f20ecd28b1992689f23391b50478",
+  soundCloudSDKURI = "http://connect.soundcloud.com/sdk.js",
+  soundCloudDebugTrackPermalink = "https://soundcloud.com/openingceremony/showtunes-steve-j-yoni-p-ss13",
+
+  init = function() {
+    var sdkEl = document.createElement("script");
+    sdkEl.src = soundCloudSDKURI;
+    document.body.appendChild(sdkEl);
+    sdkEl.addEventListener("load", onSoundCloudSDKLoad);
+  },
+
+  onSoundCloudSDKLoad = function() {
+    var prepareSound = function(soundObj, callback) {
+      if (!soundObj.track) {
+        SC.get("/resolve", { url: soundObj.url }, function(track) {
+          console.log("resolved track data: ", track);
+          soundObj.track = track;
+          prepareSound(soundObj, callback);
+        });
+        return false;
+      }
+      
+      if (!soundObj.loadedSound) {
+        SC.stream("/tracks/"+soundObj.track.id, function(loadedSound) {
+          console.log("loaded sound data: ", loadedSound);
+          soundObj.loadedSound = loadedSound;
+          prepareSound(soundObj, callback);
+        });
+        return false;
+      }
+      
+      if (callback) callback(soundObj);
+      return true;
+    };
+
+    var playSound = function(soundObj) {
+      prepareSound(soundObj, function() {
+        if (soundObj.loadedSound.playState === 1 && !soundObj.loadedSound.paused) return;
+        soundObj.loadedSound.play();
+      });
+    };
+
+    var pauseSound = function(soundObj, isPrepared) {
+      prepareSound(soundObj, function() {
+        if (soundObj.loadedSound.paused) return;
+        soundObj.loadedSound.pause();
+      });
+    };
+
+    var stopSound = function(soundObj, isPrepared) {
+      prepareSound(soundObj, function() {
+        if (soundObj.loadedSound.playState === 0) return;
+        soundObj.loadedSound.stop();
+      });
+    };
+    
+    mySounds.prepareAll = function() {
+      mySounds.forEach(function(soundObj) {
+        prepareSound(soundObj);
+      });
+    };
+    
+    mySounds.playAll = function() {
+      mySounds.forEach(function(soundObj) {
+        playSound(soundObj);
+      });
+    };
+    
+    mySounds.pauseAll = function() {
+      mySounds.forEach(function(soundObj) {
+        pauseSound(soundObj);
+      });
+    };
+
+    mySounds.stopAll = function() {
+      mySounds.forEach(function(soundObj) {
+        stopSound(soundObj);
+      });
+    };
+
+    SC.initialize({ client_id: soundCloudClientID });
+    mySounds.push({ url: soundCloudDebugTrackPermalink });
+    mySounds.playAll();
+  };
